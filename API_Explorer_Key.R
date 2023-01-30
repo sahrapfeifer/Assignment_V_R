85d85abb7b19d559468ba256d391df568309b18d
sahrapfeifer <sahra.pfeifer@gmx.de>
2023-01-30 20:55:25 +0100

    Delete API_Explorer_Key.R

--- API_Explorer_Key.R
+++
@@ -1,8 +0,0 @@
-###########################################################
-
-#                     API Explorer Key                    #
-
-###########################################################
-# Sollen wir den dann separat hochladen?
-
-TM_expl_key <- "7elxdku9GGG5k8j0Xm8KWdANDgecHMV0"
