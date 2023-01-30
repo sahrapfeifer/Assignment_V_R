e55070b6f870589a3615be0b702559edec7b8965
sahrapfeifer <sahra.pfeifer@gmx.de>
2023-01-30 20:12:46 +0100

    some little  corrections

---
+++ API_Explorer_Key.R
@@ -0,0 +1,8 @@
+###########################################################
+
+#                     API Explorer Key                    #
+
+###########################################################
+# Sollen wir den dann separat hochladen?
+
+TM_expl_key <- "7elxdku9GGG5k8j0Xm8KWdANDgecHMV0"
